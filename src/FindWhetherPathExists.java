e8ae84236acd8d957bb87ca314f5ab446cd6d8af
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-03 23:36:15 -0700

    coded a recursive call for the main function to find the path from source to the actual destination, where 1 is source, 2 is the destination, 3, is the empty shell, and 0 is the wall to prevent it from travelling

--- src/FindWhetherPathExists.java
+++ src/FindWhetherPathExists.java
@@ -1,6 +1,49 @@
 public class FindWhetherPathExists {
-   
 
+
+
+    static boolean dfs(int sr, int sc, int n, int m, int[][] grid){
+        if(sr<0 || sc<0 || sr== n|| sc ==m|| grid[sr][sc]==0){
+            return false;
+        }
+        if(grid[sr][sc]==2){
+            return true;
+        }
+
+        grid[sr][sc] = 0;
+
+     boolean a=   dfs(sr+1, sc, n, m , grid);
+    boolean b=     dfs(sr-1, sc, n, m , grid);
+      boolean c=  dfs(sr, sc+1, n, m , grid);
+       boolean d= dfs(sr, sc-1, n, m , grid);
+
+       return (a||b||c||d);
+    }
+
+public static boolean findWhetherPathExists(int [][] grid){
+
+    int n = grid.length;
+    int m = grid[0].length;
+
+    int sr=0, sc=0;
+
+    for(int i =0; i<n; i++){
+        for(int j=0; j<m; j++){
+            if(grid[i][j]== 1){
+                sr=i;
+                sc=j;
+                break;
+            }
+        }
+    }
+
+    return dfs(sr, sc, n, m, grid);
+
+
+
+
+
+}
 
 
 
