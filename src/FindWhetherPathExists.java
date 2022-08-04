b6c9c97d213c8850eb4408b2c092122d5b181ae1
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-08-03 23:41:15 -0700

    added comments for qualitative understanding in future

--- src/FindWhetherPathExists.java
+++ src/FindWhetherPathExists.java
@@ -3,6 +3,7 @@ public class FindWhetherPathExists {
 
 
     static boolean dfs(int sr, int sc, int n, int m, int[][] grid){
+        //if the element is out of the grid or its 0 then return false, if its 2 then return true
         if(sr<0 || sc<0 || sr== n|| sc ==m|| grid[sr][sc]==0){
             return false;
         }
@@ -10,23 +11,31 @@ public class FindWhetherPathExists {
             return true;
         }
 
+        // update the grid everytime during the search to 0, so that it cannot come back
         grid[sr][sc] = 0;
 
+
+        //search up, down, right and left for the result
      boolean a=   dfs(sr+1, sc, n, m , grid);
     boolean b=     dfs(sr-1, sc, n, m , grid);
       boolean c=  dfs(sr, sc+1, n, m , grid);
        boolean d= dfs(sr, sc-1, n, m , grid);
 
+
+       // if either of the search turns to be true then return true
        return (a||b||c||d);
     }
 
 public static boolean findWhetherPathExists(int [][] grid){
+        //get length of row and column
 
     int n = grid.length;
     int m = grid[0].length;
 
     int sr=0, sc=0;
 
+    // find the row and column number of the source, and then run a dfs to find the destination
+    //return true if you find it return false otherwise
     for(int i =0; i<n; i++){
         for(int j=0; j<m; j++){
             if(grid[i][j]== 1){
